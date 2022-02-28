2c490059cdb174a1e4573f0fed2ee5a0d31ad329
Ant <it@me.org>
2022-02-28 07:11:50 -0600

    Add Workouts to domain

---
+++ src/main/scala/Domain/Workouts.scala
@@ -0,0 +1,15 @@
+package Domain
+
+import java.util.UUID
+import java.util.UUID.randomUUID
+import com.github.nscala_time.time.Imports._
+
+case class Workout(workoutId: WorkoutId, date: DateTime, variation: WorkoutVariation, owner: UserId)
+
+opaque type WorkoutId = UUID
+
+object WorkoutId:
+  def apply(id: UUID): WorkoutId = id
+
+enum WorkoutVariation:
+  case UpperA, UpperB, UpperC, LowerA, LowerB, LowerC
