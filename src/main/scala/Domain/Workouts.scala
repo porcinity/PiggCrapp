872484517091dd30e0d4509afb027e0362ad85e0
Ant <it@me.org>
2022-05-09 16:41:46 -0500

    Start refactor of Workout model

--- src/main/scala/Domain/Workouts.scala
+++ src/main/scala/Domain/Workouts.scala
@@ -3,31 +3,37 @@ package Domain
 import java.time.LocalDate
 import java.util.UUID
 import java.util.UUID.randomUUID
-
-case class Workout(
-    workoutId: WorkoutId,
-    date: LocalDate,
-    variation: WorkoutVariation,
-    exercises: List[Exercise],
-    owner: UserId
-)
+import eu.timepit.refined.types.string.NonEmptyFiniteString
+import io.circe.Codec
+import eu.timepit.refined.api._
+import eu.timepit.refined.cats.CatsRefinedTypeOpsSyntax
+import org.latestbit.circe.adt.codec.JsonTaggedAdt
 
 object Workout {
 
-  def apply(variation: WorkoutVariation, userId: UserId): Workout = {
-    val id = WorkoutId(randomUUID())
-    val date = LocalDate.now()
-    val exercises = List[Exercise]()
+  case class Workout(
+      workoutId: WorkoutId,
+      date: LocalDate,
+      variation: WorkoutVariation,
+      exercises: List[Exercise],
+      owner: UserId
+  ) derives Codec.AsObject
 
-    new Workout(id, date, variation, exercises, userId)
+  type WorkoutId = NonEmptyFiniteString[30]
+
+  object WorkoutId
+      extends RefinedTypeOps[WorkoutId, String]
+      with CatsRefinedTypeOpsSyntax
+
+  enum WorkoutVariation
+      derives JsonTaggedAdt.PureEncoder,
+        JsonTaggedAdt.PureDecoder {
+    case UpperA, UpperB, UpperC, LowerA, LowerB, LowerC
   }
 
+  type UserId = NonEmptyFiniteString[20]
+
+  object UserId
+      extends RefinedTypeOps[UserId, String]
+      with CatsRefinedTypeOpsSyntax
 }
-
-opaque type WorkoutId = UUID
-
-object WorkoutId:
-  def apply(id: UUID): WorkoutId = id
-
-enum WorkoutVariation:
-  case UpperA, UpperB, UpperC, LowerA, LowerB, LowerC
