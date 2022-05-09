52c0d683e77a128c377474b5e65d77926092ebec
Ant <it@me.org>
2022-05-09 16:13:26 -0500

    Refactor User to use Refined types

--- src/main/scala/Domain/User.scala
+++ src/main/scala/Domain/User.scala
@@ -1,73 +1,44 @@
 package Domain
 
+import eu.timepit.refined.api._
 import java.time.*
 
 import java.util.UUID
+import eu.timepit.refined.types.string.NonEmptyFiniteString
+import eu.timepit.refined.cats.CatsRefinedTypeOpsSyntax
+import eu.timepit.refined.types.numeric.NonNegInt
 
-case class User(userId: UserId,
-                userName: UserName,
-                age: Age,
-                weight: UserWeight,
-                createdDate: LocalDate)
+object User {
 
-object User:
-  def apply(name: UserName, age: Age, weight: UserWeight): User =
-    val id = UserId(UUID.randomUUID())
-    val date = LocalDate.now()
-    
-    new User(id, name, age, weight, date)
+  case class User(
+      userId: UserId,
+      userName: UserName,
+      age: Age,
+      weight: UserWeight,
+      createdDate: CreatedDate
+  )
 
-opaque type UserId = UUID
+  type UserId = NonEmptyFiniteString[30]
 
-object UserId:
-  def apply(id: UUID): UserId = id
+  object UserId
+      extends RefinedTypeOps[UserId, String]
+      with CatsRefinedTypeOpsSyntax
 
-opaque type UserName = String
+  type UserName = NonEmptyFiniteString[20]
 
-object UserName:
-  def apply(value: String): Either[String, UserName] = value match
-    case Empty() => Left("Name cannot be empty.")
-    case TooLongName() => Left("Name cannot be over 30 characters.")
-    case TooShortName() => Left("Name cannot be fewer than 3 characters.")
-    case NumbersOrCharsName() => Left("Name cannot contain numbers or special characters.")
-    case _ => Right(value)
+  object UserName
+      extends RefinedTypeOps[UserName, String]
+      with CatsRefinedTypeOpsSyntax
 
-opaque type Age = Int
+  type Age = NonNegInt
 
-object Age:
-  def apply(age: Int): Either[String, Age] = age match
-    case TooOld() => Left("Age cannot be greater than 100.")
-    case TooYoung() => Left("Age must be greater than 18.")
-    case _ => Right(age)
+  object Age extends RefinedTypeOps[Age, Int] with CatsRefinedTypeOpsSyntax
 
-opaque type UserWeight = Double
+  type UserWeight = NonNegInt
 
-object UserWeight:
-  def apply(weight: Double): Either[String, UserWeight] = weight match
-    case TooHeavyUser() => Left("Weight must be less than 400lbs.")
-    case TooLightUser() => Left("Weight cannot be less than 70lbs.")
-    case _ => Right(weight)
+  object UserWeight
+      extends RefinedTypeOps[UserWeight, Int]
+      with CatsRefinedTypeOpsSyntax
 
-// Name extractors
-object TooShortName:
-  def unapply(x: String): Boolean = x.length < 3 && x.nonEmpty
-
-object TooLongName:
-  def unapply(x: String): Boolean = x.length > 30
-
-object NumbersOrCharsName:
-  def unapply(x: String): Boolean = !x.matches("^[a-zA-Z]+$")
-
-// Age extractors
-object TooOld:
-  def unapply(x: Int): Boolean = x > 100
-
-object TooYoung:
-  def unapply(x: Int): Boolean = x < 18
-
-// Weight extractors
-object TooHeavyUser:
-  def unapply(x: Double): Boolean = x > 400
-
-object TooLightUser:
-  def unapply(x: Double): Boolean = x < 70
+  type CreatedDate = LocalDate
+}
