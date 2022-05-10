7dfe1c7ba740d62c963927fc28e952075f4540b3
Ant <it@me.org>
2022-05-10 13:56:33 -0500

    Add UserDto with toDomain method

--- src/main/scala/Domain/User.scala
+++ src/main/scala/Domain/User.scala
@@ -10,6 +10,9 @@ import eu.timepit.refined.types.numeric.NonNegInt
 import Domain.Workout.Workout
 import io.circe.refined._
 import io.circe.Codec
+import cats.data.*
+import cats.syntax.all.*
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 
 object User {
 
@@ -17,7 +20,7 @@ object User {
       userId: UserId,
       userName: UserName,
       age: Age,
-      workouts: List[Workout],
+      // workouts: List[Workout],
       weight: UserWeight,
       createdDate: CreatedDate
   ) derives Codec.AsObject
@@ -44,5 +47,44 @@ object User {
       extends RefinedTypeOps[UserWeight, Int]
       with CatsRefinedTypeOpsSyntax
 
-  type CreatedDate = LocalDate
+  opaque type CreatedDate = LocalDate
+
+  object CreatedDate {
+
+    def apply(date: LocalDate): EitherNec[String, CreatedDate] = date.rightNec
+
+  }
+
+  final case class UserDto(
+      name: String,
+      age: Int,
+      weight: Int
+  )
+
+  object UserDto {
+
+    def toDomain(dto: UserDto): Either[NonEmptyChain[String], User] = {
+      val id = NanoIdUtils.randomNanoId()
+      val date = CreatedDate(LocalDate.now)
+
+      (
+        UserId.from(id).toEitherNec,
+        UserName
+          .from(dto.name)
+          .leftMap(_ => "Username must be 30 characters or fewer.")
+          .toEitherNec,
+        Age
+          .from(dto.age)
+          .leftMap(_ => "User's age must be at least 18.")
+          .toEitherNec,
+        UserWeight
+          .from(dto.weight)
+          .leftMap(_ => "Weight cannot be a negative number.")
+          .toEitherNec,
+        date
+      ).parMapN(User.apply)
+    }
+
+  }
+
 }
