134b831b046fa5fb8dd55a9b6dddf3b29a9d1463
Ant <it@me.org>
2022-05-21 15:44:56 -0500

    Modify Users algebra

--- src/main/scala/io/porcinity/piggcrapp/Persistence/Users.scala
+++ src/main/scala/io/porcinity/piggcrapp/Persistence/Users.scala
@@ -1,10 +1,11 @@
 package io.porcinity.piggcrapp.Persistence
 
 import io.porcinity.piggcrapp.Domain.User.User
+import io.porcinity.piggcrapp.Domain.User.UserId
 
 trait Users[F[_]] {
   def findAllUsers: F[List[User]]
-  def findUserById: F[User]
+  def findUserById(id: UserId): F[Option[User]]
   def create(user: User): F[User]
   def update(user: User): F[User]
   def delete(user: User): F[Option[User]]
