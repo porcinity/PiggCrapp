d36f700a7831108a4f156d75209b9eda66c38baa
Ant <it@me.org>
2022-05-24 12:39:48 -0500

    Update Workout tests

--- src/test/scala/WorkoutUtilsTests.scala
+++ src/test/scala/WorkoutUtilsTests.scala
@@ -1,16 +1,36 @@
-import Domain.*
+import io.porcinity.piggcrapp.Domain.Workout.*
+import io.porcinity.piggcrapp.Domain.User.*
 import org.scalatest.wordspec.*
 
 import java.util.UUID
+import java.time.LocalDate
+import org.scalatest.Inside
 
-class WorkoutUtilsTests extends AnyWordSpec:
+class WorkoutUtilsTests extends AnyWordSpec with Inside {
+
   "A Workout" should {
-    "be created with valid input." in {
-      val variation = WorkoutVariation.UpperC
-      val userId = UserId(UUID.randomUUID())
 
-      val workout = Workout(variation, userId)
-      assert(workout.variation == WorkoutVariation.UpperC)
-      assert(workout.variation != WorkoutVariation.LowerA)
+    "not be created with invalid input" in {
+      val variation = "UpperD"
+      val workoutDto = WorkoutDto(LocalDate.now, variation)
+      val result = WorkoutDto.toDomain(workoutDto, "")
+
+      inside(result) { case Left(e) =>
+        e.contains("Invalid variationss.") && e.length == 2
+      }
+
     }
+
+    "be created with valid input." in {
+      val variation = "UpperC"
+      val userId = UserId.unsafeFrom("1234")
+      val workout = WorkoutDto(LocalDate.now, variation)
+      val result = WorkoutDto.toDomain(workout, "1234")
+
+      inside(result) { case Right(w) =>
+        w.variation == WorkoutVariation.UpperC && w.user == userId
+      }
+    }
+    
   }
+}
